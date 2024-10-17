4424e4a133c6992bf036382c3681070836461a55
muca_isuf06 <muca.isuf06@gmail.com>
2024-10-17 07:47:18 +0000

    refactored

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ def hello_http(request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+        Returns:
+        The response text.
     """
     request_json = request.get_json(silent=True)
     request_args = request.args
@@ -21,3 +18,4 @@ def hello_http(request):
     else:
         name = 'World, from CI/CD'
     return 'Hello {}!'.format(name)
+
